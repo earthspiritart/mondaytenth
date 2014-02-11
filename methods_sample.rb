5e6fdb10d742e3dc9d603e1b8bae6025265d29d8
David H <djh@infowest.com>
2014-02-11 07:28:33 -0800

    final

--- methods_sample.rb
+++ methods_sample.rb
@@ -1,6 +1,8 @@
-#http://apidock.com/ruby/String/split
+#split - http://apidock.com/ruby/String/split
 "The rain falls mainly on the plain".split(" ")
-#join
+#join - http://apidock.com/ruby/Array/join
+["The", "rain", "falls", "mainly", "on", "the",
+ "plain"].join(" ")
 # # #@var is an instance variable, which 
 # # will be visible in all methods in an instance of the class. var is a local variable, which will only be 
 # visible in the scope it is defined.
